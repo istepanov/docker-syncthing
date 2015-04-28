1ed43bfc7186d2918eda61b05bf2290e48cf1db5
Ryan McGuire <ryan@enigmacurry.com>
2015-04-28 11:38:53 -0400

    Fixes permission errors for automatic upgrade.
    Moves the syncthing binary from / to /home/syncthing/ and changes
    ownership to the syncthing user.

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ CONFIG_FOLDER="$HOME/.config/syncthing"
 CONFIG_FILE="$CONFIG_FOLDER/config.xml"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    /syncthing -generate="$CONFIG_FOLDER"
+    $HOME/syncthing -generate="$CONFIG_FOLDER"
     xmlstarlet ed -L -u "/configuration/gui/address" -v "0.0.0.0:8080" "$CONFIG_FILE"
 fi
 
 chown -R syncthing:syncthing "$HOME"
 
-exec su - syncthing -c /syncthing
+exec su - syncthing -c $HOME/syncthing
