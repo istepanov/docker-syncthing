c518f5659fa6b9e600ccb2831c8dec908b83703c
Ilya Stepanov <dev@ilyastepanov.com>
2015-06-14 23:16:05 -0700

    Always check Web UI settings when container starts

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@ CONFIG_FILE="$CONFIG_FOLDER/config.xml"
 
 if [ ! -f "$CONFIG_FILE" ]; then
     $HOME/syncthing -generate="$CONFIG_FOLDER"
-    xmlstarlet ed -L -u "/configuration/gui/address" -v "0.0.0.0:8384" "$CONFIG_FILE"
 fi
 
+xmlstarlet ed -L -u "/configuration/gui/address" -v "0.0.0.0:8384" "$CONFIG_FILE"
 chown -R syncthing:syncthing "$HOME"
 
 exec su - syncthing -c $HOME/syncthing
